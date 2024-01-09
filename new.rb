0167b156bb633eab24fc761e768bb1dcfc803f39
takagi2943 <154996725+takagi2943@users.noreply.github.com>
2024-01-09 08:37:17 +0000

    演習問題【5】

--- new.rb
+++ new.rb
@@ -1,33 +1,7 @@
 class Car
-  def move(direction, distance)
-    self.turn(direction)
-    self.run(distance)
-  end
-
-  def turn(direction)
+  def self.turn(direction)
     puts "#{direction}に曲がります。"
   end
-
-  def run(distance)
-    puts "車で#{distance}キロ走ります。"
-  end
-
 end
 
-
-car = Car.new
-car.move("右", 5)
-
-# クラスメソッド
-class Car
-  def self.run(distance)
-    puts "車で#{distance}キロ走ります。"
-  end
-end
-
-Car.run(10)
-
-# レシーバのself
-# メソッドを呼び出したオブジェクト自身のことを指します。
-# Rubyの用語というわけではなく、一般にも使われる用語です。
-# ( . )の左側の部分がレシーバと覚えてもいいですね。
+Car.turn("右")
